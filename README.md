205d66166b12d8a9b1609ce23ebd1ff245fea324
Adrian Ruiz <adrianruiz0208@gmail.com>
2025-05-06 21:46:41 -0500

    Finalización de ajustes en proyecto y mejora de UX/UI

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# Proyecto #4 - Redes Bayesianas
+# Proyecto #3 - Redes Bayesianas
 
 Para ejecutar el proyecto se debe tener ***Python*** instalado
 
 ## Instalar dependencias
 ```
-pip install pgmy
+pip install pgmy rich
+```
+
+## Ejecutar programa
+```
+python generico.py
 ```
--- generico.py
+++ generico.py
@@ -1,4 +1,8 @@
 import json
+
+from rich import print
+from rich.prompt import Prompt
+
 from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
@@ -41,31 +45,121 @@ def cargar_red_desde_json(ruta):
     else:
         raise ValueError("Modelo inválido")
 
-# Cargar modelo
-modelo = cargar_red_desde_json("red.json")
+print("[bold red]PROYECTO 3 - REDES BAYESIANAS[/bold red]\n")
+print("Integrantes:")
+print("1. Juan Bermudez")
+print("2. Julian Espinoza")
+print("3. Julian Ramos")
+print("4. Adrian Ruiz\n")
 
-# ----------------- Realizar inferencia ------------------
-inferencia = VariableElimination(modelo)
+opcion = Prompt.ask("[italic blue]Seleccione el ejercicio a probar[/italic blue] ([bold green]0[/bold green] = clase, [bold green]1[/bold green] = ejercicio 1, [bold green]2[/bold green] = ejercicio 2)")
 
-# Enunciado:
-# Sabemos que hay lluvia ligera y no hay mantenimientos programados en las vías férreas. 
-# Nos gustaría saber las probabilidades de llegar a la reunión y de fallar a la reunión.
+print("\n------------------\n")
 
-resultado = inferencia.query(
-    variables=['Appointment'],
-    evidence={'Rain': 'light', 'Maintenance': 'no'}
-)
+if opcion == '0':
+    # ----------------- Ejercicio clase ------------------
+    print("[bold italic red]EJERCICIO CLASE[/bold italic red]\n")
+    # Cargar modelo
+    modelo = cargar_red_desde_json("red.json")
 
-print("Query 1: \n", resultado, "\n")
+    inferencia = VariableElimination(modelo)
 
-# Enunciado:
-# Sabemos que el tren viene con retraso, y no está lloviendo. 
-# Nos gustaría saber las probabilidades de llegar a la reunión y de fallar a la reunión.
+    # Enunciado:
+    # Sabemos que hay lluvia ligera y no hay mantenimientos programados en las vías férreas. 
+    # Nos gustaría saber las probabilidades de llegar a la reunión y de fallar a la reunión.
 
-resultado2 = inferencia.query(
-    variables=['Appointment'],
-    evidence={'Train': 'delayed', 'Rain': 'none'}
-)
+    resultado = inferencia.query(
+        variables=['Appointment'],
+        evidence={'Rain': 'light', 'Maintenance': 'no'}
+    )
 
-print("Query 2: \n", resultado2, "\n")
-# --------------------------------------------------------
+    print("Sabemos que hay lluvia ligera y no hay mantenimientos programados en las vias ferreas. Nos gustaria saber las probabilidades de llegar a la reunion y de fallar a la reunion: \n", resultado, "\n")
+
+    # Enunciado:
+    # Sabemos que el tren viene con retraso, y no está lloviendo. 
+    # Nos gustaría saber las probabilidades de llegar a la reunión y de fallar a la reunión.
+
+    resultado2 = inferencia.query(
+        variables=['Appointment'],
+        evidence={'Train': 'delayed', 'Rain': 'none'}
+    )
+
+    print("Sabemos que el tren viene con retraso, y no esta lloviendo. Nos gustaria saber las probabilidades de llegar a la reunion y de fallar a la reunion: \n", resultado2, "\n")
+    # --------------------------------------------------------
+
+elif opcion == '1':
+    # ----------------- EJERCICIO 1 ------------------
+    print("[bold italic red]EJERCICIO 1[/bold italic red]\n")
+    # Cargar modelo
+    modelo = cargar_red_desde_json("ejercicio1.json")
+    
+    inferencia = VariableElimination(modelo)
+
+    # Enunciado:
+    # ¿Cual es la probabilidad de ser admitido a una universidad dado que el examen es dificil?
+
+    resultado = inferencia.query(
+        variables=['Admission'],
+        evidence={'Exam level': 'dificil'}
+    )
+
+    print("¿Cual es la probabilidad de ser admitido a una universidad dado que el examen es dificil?: \n", resultado, "\n")
+
+    # Enunciado:
+    # ¿Cual es la probabilidad de tener un puntaje alto de aptitud?
+
+    resultado2 = inferencia.query(
+        variables=['Apti. score']
+    )
+
+    print("¿Cual es la probabilidad de tener un puntaje alto de aptitud?: \n", resultado2, "\n")
+
+    # Enunciado:
+    # ¿Cual es la distribucion de la admision?
+
+    resultado3 = inferencia.query(
+        variables=['Admission']
+    )
+
+    print("¿Cual es la distribucion de la admision?: \n", resultado3, "\n")
+    # --------------------------------------------------------
+
+elif opcion == '2':
+    # ----------------- EJERCICIO 2 ------------------
+    print("[bold italic red]EJERCICIO 2[/bold italic red]\n")
+    # Cargar modelo
+    modelo = cargar_red_desde_json("ejercicio2.json")
+    
+    inferencia = VariableElimination(modelo)
+
+    # Enunciado:
+    # ¿Cual es la probabilidad de ser feliz dado que asiste a fiestas frecuentemente y es terriblemente inteligente pero no muy creativo?
+
+    resultado = inferencia.query(
+        variables=['Happy'],
+        evidence={'Party': 'si', 'Smart': 'si', 'Creative': 'no'}
+    )
+
+    print("¿Cual es la probabilidad de ser feliz dado que asiste a fiestas frecuentemente y es terriblemente inteligente pero no muy creativo?: \n", resultado, "\n")
+
+    # Enunciado:
+    # ¿Cual es la probabilidad de ser feliz dado que no asiste a fiestas, tiene buenas calificaciones en sus tareas y aprobo el proyecto del curso?
+
+    resultado2 = inferencia.query(
+        variables=['Happy'],
+        evidence={'Party': 'no', 'HW': 'si', 'Project': 'si'}
+    )
+
+    print("¿Cual es la probabilidad de ser feliz dado que no asiste a fiestas, tiene buenas calificaciones en sus tareas y aprobo el proyecto del curso?: \n", resultado2, "\n")
+
+    # Enunciado:
+    # ¿Cual es la distribucion de la felicidad?
+
+    resultado3 = inferencia.query(
+        variables=['Happy']
+    )
+
+    print("¿Cual es la distribucion de la felicidad?: \n", resultado3, "\n")
+    # --------------------------------------------------------
+else:
+    print("Opción no válida. Seleccione 0, 1 o 2.")
